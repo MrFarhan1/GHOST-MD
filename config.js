4cabda9146aba3c6d64f1c8b6ac770b89e3c49bb
MrFarhan1 <alifarhanwatch52@gmail.com>
2024-09-23 21:29:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "1mMkAIAS#iFtloU4iR8n02z3jvyjpb1eAtD1NdBZKlVxd3d53RzY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 MONGODB: process.env.MONGODB || "",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
